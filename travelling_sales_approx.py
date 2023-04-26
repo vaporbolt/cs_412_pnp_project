388fc5eff4e7850dca04bb656692a977638bcdb5
Max Mancini <mman7840@gmail.com>
2023-04-26 10:26:16 -0400

    fixed input to match expected on approximation

--- travelling_sales_approx.py
+++ travelling_sales_approx.py
@@ -10,9 +10,14 @@ name: Seth Roper and Max Mancini
 2 3 12
 1 2 30
 
+3 3
+a b 3
+b c 4
+a c 5
+
  """
-import itertools
 import random
+import time
 
 
 def find_optimal_tsp_path(graph, src, max_iter):
@@ -30,10 +35,10 @@ def find_optimal_tsp_path(graph, src, max_iter):
         last_vertex = src
         for i in vertex_list:
             path.append(i)
-            cost += graph[i][last_vertex]
+            cost += float(graph[i][last_vertex])
             last_vertex = i
         path.append(src)
-        cost += graph[last_vertex][src]
+        cost += float(graph[last_vertex][src])
         if cost < current_best_cost:
             current_best_cost = cost
             current_best_path = path
@@ -56,15 +61,20 @@ def main():
     num_vertices = int(num_things[0])
     num_edges = int(num_things[1])
     graph = {}
-    for i in range(num_vertices):
-        graph[i] = {}
     for i in range(num_edges):
-        edge_info = list(map(lambda x: int(x), input().split(" ")))
+        edge_info = input().split(" ")
+        if graph.get(edge_info[0]) is None:
+            graph[edge_info[0]] = {}
+        if graph.get(edge_info[1]) is None:
+            graph[edge_info[1]] = {}
         graph[edge_info[0]][edge_info[1]] = edge_info[2]
         graph[edge_info[1]][edge_info[0]] = edge_info[2]
-    opt_path, opt_cost = find_optimal_tsp_path(graph, 0, 50)
+    start = time.time()
+    opt_path, opt_cost = find_optimal_tsp_path(graph, "0", 50)
+    end = time.time()
     print(opt_path)
     print(opt_cost)
+    print(end - start)
 
 
 if __name__ == "__main__":
